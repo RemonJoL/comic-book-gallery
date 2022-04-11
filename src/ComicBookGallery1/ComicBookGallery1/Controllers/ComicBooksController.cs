f4d9dd680957cb4b19e6252c12ea2c5fb03b9b09
RemonJoL <83402319+RemonJoL@users.noreply.github.com>
2022-04-11 15:32:04 +0100

    Added Comic Books Controller

---
+++ src/ComicBookGallery1/ComicBookGallery1/Controllers/ComicBooksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicBookGallery1.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+
+    }
+}
---
+++ src/ComicBookGallery1/ComicBookGallery1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ComicBookGallery1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
